d9aecf1f914cbd9101efce9bb12e3a9989acf9dc
karrirasinmaki <karri.rasinmaki@gmail.com>
2014-02-09 18:10:31 +0200

    Splash screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,25 @@
 var MetroCoffee = (function() {
     
-    require(["utils/hammer", "utils/utils", "ui/widgets", "ui/mainView"],
-    function(hammer,          utils,         widgets,      mainView) {
+    require(["utils/hammer", "utils/utils", "ui/widgets", "ui/mainView", "domReady"],
+    function(hammer,          utils,         widgets,      mainView,      domReady) {
         
         var context = {
             Hammer: hammer,
             widgets: widgets,
             utils: utils
         };
-        
+                
         utils.load.css("font/nexa/stylesheet.css");
         utils.load.css("css/animate.css");
         mainView.init(context);
+        
+        domReady(function() {
+            var splash = document.getElementById("splashscreen");
+            widgets.addClass(splash, "animated bounceOut");
+            setTimeout(function() {
+                splash.remove();
+            }, 1000);
+        });
     });
     
 })();
